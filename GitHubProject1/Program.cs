fb3a0822d7428d704a4e4678543da962ac7dd05e
ItsSteph <steph.tronier.vip@gmail.com>
2020-11-04 13:30:14 +0100

    Add a printout

--- GitHubProject1/Program.cs
+++ GitHubProject1/Program.cs
@@ -6,7 +6,7 @@ namespace GitHubProject1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi Guthub");
         }
     }
 }
