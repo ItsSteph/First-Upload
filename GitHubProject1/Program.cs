869a819400d7f867d91cda1260d600b17f2a61a5
Steph <70627352+ItsSteph@users.noreply.github.com>
2020-11-04 11:16:54 +0100

    First Commit

---
+++ GitHubProject1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
